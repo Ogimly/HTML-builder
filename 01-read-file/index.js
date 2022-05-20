e462490a7e95cbfaefba0c5d22aefc053ad3bd02
ogimly <ogimly@gmail.com>
2022-05-20 17:42:58 +0500

    feat: 01-read-file (pipeline)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -21,8 +21,21 @@ streamRead.on('data', dataListener);
 streamRead.on('error', errorListener);
 streamRead.on('end', endListener);
 
+//
 // use pipe()
 // const fullName = path.join(__dirname, 'text.txt');
 // const streamRead = fs.createReadStream(fullName, 'utf-8');
 //
 // streamRead.pipe(stdout);
+
+//
+// use pipeline()
+// const { pipeline } = require('stream');
+
+// const fullName = path.join(__dirname, 'text.txt');
+// const streamRead = fs.createReadStream(fullName, 'utf-8');
+// pipeline(streamRead, stdout, (error) => {
+//   if (error) {
+//     stderr.write(`error reading file: ${error}`);
+//   }
+// });
